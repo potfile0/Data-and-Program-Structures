3e1d3cb01f9039dfbeac40f932e68614ddfe28e0
Sanskar <sanskarluitel51@gmail.com>
2026-01-26 09:31:39 -0800

    Lab Assignment 1 completed

--- Trivial-Program/Ksu.Cis300.TrivialProgram/UserInterface.Designer.cs
+++ Trivial-Program/Ksu.Cis300.TrivialProgram/UserInterface.Designer.cs
@@ -1,6 +1,6 @@
 namespace Ksu.Cis300.TrivialProgram
 {
-    partial class Form1
+    partial class UserInterface
     {
         /// <summary>
         ///  Required designer variable.
---
+++ Trivial-Program/Ksu.Cis300.TrivialProgram/UserInterface.cs
@@ -0,0 +1,16 @@
+/* UserInterface.cs
+ * Author: Sanskar
+ */
+using System.Windows.Forms;
+namespace Ksu.Cis300.TrivialProgram
+{
+    //A GUI for a program that opens an empty window.
+    public partial class UserInterface : Form
+    {
+        //constructs the GUI.
+        public UserInterface()
+        {
+            InitializeComponent();
+        }
+    }
+}

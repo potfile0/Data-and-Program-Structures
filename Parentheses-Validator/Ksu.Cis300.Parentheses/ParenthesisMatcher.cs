dc65e45b5b2c281215dc6dfb1d0c291cb3476530
Sanskar <sanskarluitel51@gmail.com>
2026-02-02 13:05:36 -0600

    Lab 6 completed

--- Parentheses-Validator/Ksu.Cis300.Parentheses/ParenthesisMatcher.cs
+++ Parentheses-Validator/Ksu.Cis300.Parentheses/ParenthesisMatcher.cs
@@ -1,6 +1,7 @@
 /* ParenthesisMatcher.cs
  * Author:
  */
+using System.Collections.Generic;
 namespace Ksu.Cis300.Parentheses
 {
     /// <summary>
@@ -15,7 +16,65 @@ namespace Ksu.Cis300.Parentheses
         /// <returns>Whether the string is balanced.</returns>
         public static bool IsBalanced(string s)
         {
-            throw new NotImplementedException();
+            Stack<char> stack = new Stack<char>();
+
+            foreach (char c in s)
+            {
+                if (IsOpeningParenthesis(c))
+                {
+                    stack.Push(c);
+                }else if (IsClosingParenthesis(c))
+                {
+                    if(stack.Count <= 0)
+                    {
+                        return false;
+                    }
+                    char open = stack.Peek();
+                    if (Matches(open, c))
+                    {
+                        stack.Pop();
+                    }
+                    else
+                    {
+                        return false;
+                    }
+
+                }
+            }
+            return stack.Count == 0;
+        }
+
+        /// <summary>
+        /// Determines whether the given character is an opening parenthesis.
+        /// </summary>
+        /// <param name="c">The character to check.</param>
+        /// <returns>Whether c is an opening parenthesis.</returns>
+        private static bool IsOpeningParenthesis(char c)
+        {
+            return c == '(' || c == '[' || c == '{';
+        }
+
+        /// <summary>
+        /// Determines whether the given character is a closing parenthesis.
+        /// </summary>
+        /// <param name="c">The character to check.</param>
+        /// <returns>Whether c is a closing parenthesis.</returns>
+        private static bool IsClosingParenthesis(char c)
+        {
+            return c == ')' || c == ']' || c == '}';
+        }
+
+        /// <summary>
+        /// Determines whether the given characters form a matched pair
+        /// of parentheses.
+        /// </summary>
+        /// <param name="a">The opening character.</param>
+        /// <param name="b">The closing character.</param>
+        /// <returns>Whether a and b form a matched pair of parentheses.</returns>
+        private static bool Matches(char a, char b)
+        {
+            return (a == '(' && b == ')') || (a == '[' && b == ']') ||
+                (a == '{' && b == '}');
         }
     }
 }

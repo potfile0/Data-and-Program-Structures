89bc12bc9f7d47bf230bad0e8231c4bcbf3ddd70
Sanskar <sanskarluitel51@gmail.com>
2026-02-15 19:27:36 -0800

    Buld successful, 2 errors while testing

--- Towers-of-Hanoi/KSU.CIS300.TowerOfHanoi/UserInterface.Designer.cs
+++ Towers-of-Hanoi/KSU.CIS300.TowerOfHanoi/UserInterface.Designer.cs
@@ -20,6 +20,7 @@
             base.Dispose(disposing);
         }
 
+
         #region Windows Form Designer generated code
 
         /// <summary>
@@ -28,21 +29,242 @@
         /// </summary>
         private void InitializeComponent()
         {
+            button1 = new System.Windows.Forms.Button();
+            numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            label1 = new System.Windows.Forms.Label();
+            label2 = new System.Windows.Forms.Label();
+            button2 = new System.Windows.Forms.Button();
+            button3 = new System.Windows.Forms.Button();
+            label3 = new System.Windows.Forms.Label();
+            numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            flowLayoutPanel3 = new System.Windows.Forms.FlowLayoutPanel();
+            button4 = new System.Windows.Forms.Button();
+            button5 = new System.Windows.Forms.Button();
+            button6 = new System.Windows.Forms.Button();
+            button7 = new System.Windows.Forms.Button();
+            button8 = new System.Windows.Forms.Button();
+            button9 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).BeginInit();
             SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new System.Drawing.Point(20, 11);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "New";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new System.Drawing.Point(101, 11);
+            numericUpDown1.Maximum = new decimal(new int[] { 11, 0, 0, 0 });
+            numericUpDown1.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new System.Drawing.Size(39, 23);
+            numericUpDown1.TabIndex = 1;
+            numericUpDown1.Value = new decimal(new int[] { 3, 0, 0, 0 });
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(181, 15);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(79, 15);
+            label1.TabIndex = 3;
+            label1.Text = "Move Count: ";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new System.Drawing.Point(266, 15);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(13, 15);
+            label2.TabIndex = 4;
+            label2.Text = "0";
+            // 
+            // button2
+            // 
+            button2.Location = new System.Drawing.Point(295, 7);
+            button2.Name = "button2";
+            button2.Size = new System.Drawing.Size(75, 23);
+            button2.TabIndex = 5;
+            button2.Text = "Undo";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new System.Drawing.Point(388, 7);
+            button3.Name = "button3";
+            button3.Size = new System.Drawing.Size(75, 23);
+            button3.TabIndex = 6;
+            button3.Text = "Solve";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new System.Drawing.Point(479, 13);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(39, 15);
+            label3.TabIndex = 7;
+            label3.Text = "Delay:";
+            // 
+            // numericUpDown2
+            // 
+            numericUpDown2.Location = new System.Drawing.Point(524, 9);
+            numericUpDown2.Maximum = new decimal(new int[] { 9000, 0, 0, 0 });
+            numericUpDown2.Name = "numericUpDown2";
+            numericUpDown2.Size = new System.Drawing.Size(61, 23);
+            numericUpDown2.TabIndex = 8;
+            numericUpDown2.Value = new decimal(new int[] { 500, 0, 0, 0 });
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.BottomUp;
+            flowLayoutPanel1.Location = new System.Drawing.Point(3, 54);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new System.Drawing.Size(175, 338);
+            flowLayoutPanel1.TabIndex = 9;
+            // 
+            // flowLayoutPanel2
+            // 
+            flowLayoutPanel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            flowLayoutPanel2.FlowDirection = System.Windows.Forms.FlowDirection.BottomUp;
+            flowLayoutPanel2.Location = new System.Drawing.Point(201, 54);
+            flowLayoutPanel2.Name = "flowLayoutPanel2";
+            flowLayoutPanel2.Size = new System.Drawing.Size(178, 338);
+            flowLayoutPanel2.TabIndex = 10;
+            // 
+            // flowLayoutPanel3
+            // 
+            flowLayoutPanel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            flowLayoutPanel3.FlowDirection = System.Windows.Forms.FlowDirection.BottomUp;
+            flowLayoutPanel3.Location = new System.Drawing.Point(399, 54);
+            flowLayoutPanel3.Name = "flowLayoutPanel3";
+            flowLayoutPanel3.Size = new System.Drawing.Size(186, 338);
+            flowLayoutPanel3.TabIndex = 11;
+            // 
+            // button4
+            // 
+            button4.Location = new System.Drawing.Point(12, 398);
+            button4.Name = "button4";
+            button4.Size = new System.Drawing.Size(75, 34);
+            button4.TabIndex = 12;
+            button4.Text = "Move Left";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new System.Drawing.Point(204, 398);
+            button5.Name = "button5";
+            button5.Size = new System.Drawing.Size(75, 34);
+            button5.TabIndex = 13;
+            button5.Text = "Move Left";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new System.Drawing.Point(409, 398);
+            button6.Name = "button6";
+            button6.Size = new System.Drawing.Size(75, 34);
+            button6.TabIndex = 14;
+            button6.Text = "Move Left";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // button7
+            // 
+            button7.Location = new System.Drawing.Point(93, 398);
+            button7.Name = "button7";
+            button7.Size = new System.Drawing.Size(83, 34);
+            button7.TabIndex = 15;
+            button7.Text = "Move Right";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
+            // 
+            // button8
+            // 
+            button8.Location = new System.Drawing.Point(285, 398);
+            button8.Name = "button8";
+            button8.Size = new System.Drawing.Size(85, 34);
+            button8.TabIndex = 16;
+            button8.Text = "Move Right";
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += button8_Click;
+            // 
+            // button9
+            // 
+            button9.Location = new System.Drawing.Point(490, 398);
+            button9.Name = "button9";
+            button9.Size = new System.Drawing.Size(80, 34);
+            button9.TabIndex = 17;
+            button9.Text = "Move Right";
+            button9.UseVisualStyleBackColor = true;
+            button9.Click += button9_Click;
             // 
             // UserInterface
             // 
-            AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            ClientSize = new System.Drawing.Size(1010, 869);
+            BackColor = System.Drawing.SystemColors.ButtonFace;
+            ClientSize = new System.Drawing.Size(593, 436);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(flowLayoutPanel3);
+            Controls.Add(flowLayoutPanel2);
+            Controls.Add(flowLayoutPanel1);
+            Controls.Add(numericUpDown2);
+            Controls.Add(label3);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(numericUpDown1);
+            Controls.Add(button1);
             FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            Margin = new System.Windows.Forms.Padding(1);
             Name = "UserInterface";
             Text = "Tower of Hanoi";
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
     }
 }
 
--- Towers-of-Hanoi/KSU.CIS300.TowerOfHanoi/UserInterface.cs
+++ Towers-of-Hanoi/KSU.CIS300.TowerOfHanoi/UserInterface.cs
@@ -12,16 +12,339 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Serialization;
 
 namespace KSU.CIS300.TowerOfHanoi
 {
-   
+
     public partial class UserInterface : Form
     {
-      
+        public int MoveCount { get; private set; }
+
+        public bool TestMode { get; set; }
+
+        public Stack<int> PegA { get; set; }
+
+        public Stack<int> PegB { get; set; }
+
+        public Stack<int> PegC { get; set; }
+
+        private int _discCount;
+
+
+        //to keep track of the manual move, made 2 stacks that store entire stacks
+        Stack<Stack<int>> moveFrom = new Stack<Stack<int>>();
+        Stack<Stack<int>> moveTo = new Stack<Stack<int>>();
+
+        public void NewPuzzle(int count)
+        {
+            MoveCount = 0;
+            PegA.Clear();
+            PegB.Clear();
+            PegC.Clear();
+            CleanPanel(flowLayoutPanel1);
+            CleanPanel(flowLayoutPanel2);
+            CleanPanel(flowLayoutPanel3);
+            _discCount = count;
+
+            for (int i = _discCount; i > 0; i--)
+            {
+                PegA.Push(i);
+                DrawDisc(i, flowLayoutPanel1);
+            }
+
+
+        }
         public UserInterface()
         {
             InitializeComponent();
-        }       
+            PegA = new Stack<int>();
+            PegB = new Stack<int>();
+            PegC = new Stack<int>();
+            int count = (int)numericUpDown1.Value;
+            NewPuzzle(count);
+        }
+
+        private void DrawDisc(int number, FlowLayoutPanel panel)
+        {
+            Label disc = new Label();
+            disc.Width = (int)(panel.Width / (_discCount - number + 1.25));
+            Padding temp = disc.Margin;
+            temp.Left = (int)((panel.Width - disc.Width) / 2);
+            temp.Right = (int)((panel.Width - disc.Width) / 2);
+
+            disc.Margin = temp;
+
+
+
+            if (_discCount % 2 == 0)
+            {
+                disc.BackColor = Color.Gray;
+            }
+            else
+            {
+                disc.BackColor = Color.Red;
+            }
+
+            disc.Text = number.ToString();
+
+            disc.TextAlign = ContentAlignment.MiddleCenter;
+            panel.Controls.Add(disc);
+        }
+
+        private void CleanPanel(FlowLayoutPanel panel)
+        {
+            Stack<Control> controls = new Stack<Control>();
+            foreach (Control control in panel.Controls)
+            {
+                controls.Push(control);
+            }
+            while (controls.Count > 0)
+            {
+                Control x = controls.Pop();
+                panel.Controls.Remove(x);
+                x.Dispose();
+
+            }
+        }
+
+       
+
+        private bool UpdatePanelHelper(
+            FlowLayoutPanel source, Stack<int> sourceStack,
+            FlowLayoutPanel dest, Stack<int> destStack,
+            FlowLayoutPanel alt, Stack<int> altStack)
+        {
+            Control temp = null;
+
+            if (source.Controls.Count != sourceStack.Count)
+            {
+                if (source.Controls.Count > sourceStack.Count)
+                {
+                    temp = source.Controls[source.Controls.Count - 1];
+                    source.Controls.RemoveAt(source.Controls.Count - 1);
+                    source.Update();
+
+                    if (dest.Controls.Count < destStack.Count)
+                    {
+                        dest.Controls.Add(temp);
+                        dest.Update();
+                    }
+                    else if (alt.Controls.Count < altStack.Count)
+                    {
+                        alt.Controls.Add(temp);
+                        alt.Update();
+                    }
+                }
+
+                if (source.Controls.Count < sourceStack.Count)
+                {
+                    if (temp != null)
+                    {
+                        source.Controls.Add(temp);
+                        source.Update();
+                    }
+                }
+            }
+            return true;
+        }
+
+        private void UpdatePanels()
+        {
+            UpdatePanelHelper(flowLayoutPanel1, PegA, flowLayoutPanel2, PegB, flowLayoutPanel3, PegC);
+        }
+
+        public bool CheckMove(Stack<int> from, Stack<int> to)
+        {
+            if (from.Count == 0)
+            {
+                return false;
+            }
+            else if (to.Count == 0)
+            {
+                return true;
+            }
+            else
+            {
+                if (from.Peek() < to.Peek())
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+
+        public bool MoveDisc(Stack<int> from, Stack<int> to)
+        {
+            if (!CheckMove(from, to))
+            {
+                return false;
+            }
+
+            int disc = from.Pop();
+            to.Push(disc);
+
+            MoveCount++;
+            label2.Text = MoveCount.ToString();
+            label2.Update();
+
+            UpdatePanels();
+
+            return true;
+        }
+
+        public void MoveEither(Stack<int> x, Stack<int> y)
+        {
+            bool moved = MoveDisc(x, y);
+
+            if (!moved)
+            {
+                MoveDisc(y, x);
+            }
+        }
+        //check to see if the tower is complete
+        public bool CheckIfWon()
+        {
+            if (PegC.Count != numericUpDown1.Value)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+
+        }
+        public void Solve(Stack<int> x, Stack<int> y, int delay)
+        {
+            while (PegC.Count != numericUpDown1.Value)
+            {
+                MoveEither(PegA, x);
+                if (CheckIfWon().Equals(true))
+                {
+                    break;
+                }
+                Thread.Sleep(delay);
+                MoveEither(PegA, y);
+                if (CheckIfWon().Equals(true))
+                {
+                    break;
+                }
+                Thread.Sleep(delay);
+                MoveEither(PegB, PegC);
+                if (CheckIfWon().Equals(true))
+                {
+                    break;
+                }
+                Thread.Sleep(delay);
+            }
+        }
+
+        //new button event handler
+        private void button1_Click(object sender, EventArgs e)
+        {
+            NewPuzzle((int)numericUpDown1.Value);
+        }
+
+        private void FlowLayoutPanelPegC_ControlAdded(object sender, ControlEventArgs e)
+        {
+            if (flowLayoutPanel3.Controls.Count == _discCount)
+            {
+                if (PegC.Count == _discCount && PegA.Count == 0 && PegB.Count == 0)
+                {
+                    if (MoveCount == (int)(Math.Pow(2, _discCount) - 1))
+                    {
+                        MessageBox.Show("You won the Game in the optimal number of moves!!!!You're a Genius!!!!");
+                    }
+                    else
+                    {
+                        MessageBox.Show("You won the Game!!!! Hoorayyyyyy!!!!");
+                    }
+                }
+            }
+
+        }
+
+        //solve button event handler
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (_discCount % 2 == 0)
+            {
+                Solve(PegB, PegC, (int)(numericUpDown2.Value));
+            }
+            else
+            {
+                Solve(PegC, PegB, (int)(numericUpDown2.Value));
+            }
+
+        }
+
+        //Move Left button under Peg A
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MoveDisc(PegA, PegC);
+            HistroyOfManualMoves(PegA, PegC);
+        }
+
+        //Move Right Button under Peg A
+        private void button7_Click(object sender, EventArgs e)
+        {
+            MoveDisc(PegA, PegB);
+            HistroyOfManualMoves(PegA, PegB);
+        }
+
+        //Move Left button under Peg B
+        private void button5_Click(object sender, EventArgs e)
+        {
+            MoveDisc(PegB, PegA);
+            HistroyOfManualMoves(PegB, PegA);
+        }
+
+        //Move Right Button under Peg B
+        private void button8_Click(object sender, EventArgs e)
+        {
+            MoveDisc(PegB, PegC);
+            HistroyOfManualMoves(PegB, PegC);
+        }
+
+        //Move Left button under Peg C
+        private void button6_Click(object sender, EventArgs e)
+        {
+            MoveDisc(PegC, PegB);
+            HistroyOfManualMoves(PegC, PegB);
+        }
+
+        //Move Right Button under Peg C
+        private void button9_Click(object sender, EventArgs e)
+        {
+            MoveDisc(PegC, PegA);
+            HistroyOfManualMoves(PegC, PegA);
+        }
+
+        //to keep the history of manual move
+        public void HistroyOfManualMoves(Stack<int> from, Stack<int> to)
+        {
+            moveFrom.Push(from);
+            moveTo.Push(to);
+        }
+        
+
+
+        //Undo button event handler
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (moveFrom.Count > 0)
+            {
+                Stack<int> lastFrom = moveFrom.Pop();
+                Stack<int> lastTo = moveTo.Pop();
+
+                MoveDisc(lastTo, lastFrom);
+
+                if (moveFrom.Count == 0)
+                {
+                    button2.Enabled = false;
+                }
+            }
+        }
     }
 }

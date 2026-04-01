2f9aaa7e946e647954ec5735145286ca6c9a9be9
Sanskar <sanskarluitel51@gmail.com>
2026-04-01 13:38:11 -0500

    lab 25 completed

--- Leftist-Tree-Priority-Queue/Ksu.Cis300.PriorityQueueLibrary/LeftistTree.ITree.cs
+++ Leftist-Tree-Priority-Queue/Ksu.Cis300.PriorityQueueLibrary/LeftistTree.ITree.cs
@@ -1,4 +1,4 @@
-/* BinaryTreeNode.ITree.cs
+/* LeftistTree.ITree.cs
  * Author: Josh Weese
  * 
  * Note: This file contains only code relevant to drawing the tree.
@@ -17,7 +17,7 @@ namespace Ksu.Cis300.PriorityQueueLibrary
     /// An immutable binary tree node that can draw itself.
     /// </summary>
     /// <typeparam name="T">The type of the elements in the tree.</typeparam>
-    public partial class BinaryTreeNode<T> : ITree
+    public partial class LeftistTree<T> : ITree
     {
         /// <summary>
         /// Gets the children of this node.

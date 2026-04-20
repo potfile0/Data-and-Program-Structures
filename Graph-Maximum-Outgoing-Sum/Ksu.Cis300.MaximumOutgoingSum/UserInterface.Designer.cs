d12b81ed768f845c513f051892852d14888fd45b
Sanskar <sanskarluitel51@gmail.com>
2026-04-20 13:44:55 -0500

    lab 32 completed

--- Graph-Maximum-Outgoing-Sum/Ksu.Cis300.MaximumOutgoingSum/UserInterface.Designer.cs
+++ Graph-Maximum-Outgoing-Sum/Ksu.Cis300.MaximumOutgoingSum/UserInterface.Designer.cs
@@ -28,12 +28,38 @@ namespace Ksu.Cis300.MaximumOutgoingSum
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            button1 = new Button();
+            openFileDialog1 = new OpenFileDialog();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(0, 0);
+            button1.Name = "button1";
+            button1.Size = new Size(116, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Read a Graph";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // UserInterface
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Name = "UserInterface";
+            Text = "Form1";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button button1;
+        private OpenFileDialog openFileDialog1;
     }
 }

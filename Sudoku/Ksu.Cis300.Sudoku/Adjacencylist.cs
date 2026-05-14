437e795a18a9abd112bc95c9db89fbe1ef680bc3
Sanskar <sanskarluitel51@gmail.com>
2026-05-14 15:04:10 -0500

    hw5 completed

---
+++ Sudoku/Ksu.Cis300.Sudoku/Adjacencylist.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ksu.Cis300.Sudoku
+{
+    /// <summary>
+    /// This enum just tracks the two directions we use for the adjacency lists
+    /// in the bipartite graph. Element has to come first or the unit tests will
+    /// break since they expect the subset lists to live at index [1].
+    /// </summary>
+    public enum AdjacencyList
+    {
+        /// <summary>
+        /// This is the direction going from a element node.
+        /// </summary>
+        Element,
+        /// <summary>
+        /// This is the direction going from a subset node.
+        /// </summary>
+        Subset
+    }
+}
---
+++ Sudoku/Ksu.Cis300.Sudoku/Decision.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Ksu.Cis300.Sudoku
+{
+    /// <summary>
+    /// Represents a decision we can make in the puzzle, basically just saying
+    /// "put this value in this cell". These are the subset nodes in the bipartite
+    /// graph. Using bytes to keep the struct small and not waste memory.
+    /// </summary>
+    public struct Decision
+    {
+        /// <summary>
+        /// The row where we're placing the value.
+        /// </summary>
+        public byte Row { get; }
+        /// <summary>
+        /// The column where we're placing the value.
+        /// </summary>
+        public byte Column { get; }
+        /// <summary>
+        /// The actual value that getting placed in the cell.
+        /// </summary>
+        public byte Value { get; }
+        /// <summary>
+        /// Just a simple constructor that takes a row, column, and value and
+        /// sets everything up, nothing fancy going on here.
+        /// </summary>
+        public Decision(byte row, byte column, byte value)
+        {
+            Row = row;
+            Column = column;
+            Value = value;
+        }
+    }
+}
